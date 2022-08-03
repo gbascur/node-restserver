d0ded999127591268facf21b9071b78b0d4072ee
“GB” <ext_gnbascur@falabella.cl>
2022-08-03 11:39:27 -0400

    rest server

---
+++ routes/usuarios.js
@@ -0,0 +1,20 @@
+
+const { Router } = require('express');
+
+const { usuariosGet, usuariosDelete, usuariosPatch, usuariosPut, usuariosPost } = require('../controllers/usuarios')
+
+const router = Router();
+
+  router.get('/', usuariosGet );
+
+  router.post('/', usuariosPost );
+
+  router.put('/:id', usuariosPut );
+
+  router.patch('/', usuariosPatch );
+ 
+
+  router.delete('/',  usuariosDelete);
+
+
+module.exports= router
